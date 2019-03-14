ee252b5aa1febc4d774a0b0c3c5643a3b33db924
Andrey Larin <lestex@gmail.com>
2019-03-14 17:29:37 +0300

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -2,7 +2,7 @@ variable "vpc_id" {}
 
 variable "name_prefix" {
   description = "The prefix for the resource names. You will probably want to set this to the name of your VPC, if you have multiple."
-  default     = ""
+  default     = "vpc"
 }
 
 variable "traffic_type" {
@@ -13,7 +13,7 @@ variable "traffic_type" {
 // workaround for not being able to do interpolation in variable defaults
 // https://github.com/hashicorp/terraform/issues/4084
 locals {
-  default_log_destination = "${var.name_prefix}-vpc-flow-log"
+  default_log_destination = "${var.name_prefix}-flow-log"
 }
 
 variable "log_destination" {
